e8821cf94790a65895aec931e67663eb09f6587c
Robert Edmonds <bobby.edmonds89@gmail.com>
2022-03-22 13:34:03 -0600

    Complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,36 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
 function App() {
+  const [isItems, setIsItems] = useState([])
+  const [isSearch, setIsSearch] = useState("")
+
+  useEffect(()=>{
+    fetch("http://localhost:6001/listings")
+    .then(resp => resp.json())
+    .then(item => setIsItems(item))
+  }, [])
+
+  function handleDeleteItem(deleteItem){
+    const updateItems = itemsToDisplay.filter(item => item.id !== deleteItem.id)
+    setIsItems(updateItems)
+  }
+
+  function handleSearchChange(searchItem){
+    setIsSearch(searchItem)
+  }
+
+  const itemsToDisplay = isItems.filter(item =>{
+    if(isSearch === "")return true;
+
+    return item.description === isSearch;
+  })
+
   return (
     <div className="app">
-      <Header />
-      <ListingsContainer />
+      <Header onSearchChange={handleSearchChange} searchName={isSearch}/>
+      <ListingsContainer displayObjects={itemsToDisplay} onDeleteItem={handleDeleteItem}/>
     </div>
   );
 }
