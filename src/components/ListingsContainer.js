42ae6d3b3d57d8a6eeff10483397504898cc6751
Robert Edmonds <bobby.edmonds89@gmail.com>
2022-03-22 13:28:00 -0600

    commit tree d7a48e96c8be605804b55e0fd6cdf7dba0c313ac

--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,11 +1,17 @@
 import React from "react";
+import ListingCard from "./ListingCard";
 // import ListingCard from "./ListingCard";
 
-function ListingsContainer() {
+function ListingsContainer({displayObjects, onDeleteItem}) {
+  console.log(displayObjects)
+  const showObjects = displayObjects.map(item => {
+    return <ListingCard key={item.id} description={item.description} image={item.image} location={item.location} item={item} onDeleteItem={onDeleteItem}/>
+  })
+
   return (
     <main>
       <ul className="cards">
-        {/* use the ListingCard component to display listings */}
+        {showObjects}
       </ul>
     </main>
   );
